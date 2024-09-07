9564cfbc191d5ed6bd591258e008a71d27d9cdc3
nyong <sptcnl@gmail.com>
2024-09-07 19:15:56 +0900

    update: added login, logout feature

--- accounts/views.py
+++ accounts/views.py
@@ -33,12 +33,26 @@ def signup(request):
     return render(request, "accounts/signup.html", context)
 
 
+@require_http_methods(["GET", "POST"])
 def login(request):
-    return render(request, "accounts/login.html")
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            next_url = request.GET.get("next") or "index"
+            return redirect(next_url)
+    else:
+        form = AuthenticationForm()
+
+    context = {"form": form}
+    return render(request, "accounts/login.html", context)
 
 
+@require_POST
 def logout(request):
-    return redirect("/")
+    if request.user.is_authenticated:
+        auth_logout(request)
+    return redirect("index")
 
 
 def delete(request):
