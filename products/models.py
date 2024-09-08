d4de12f68d2893b38da01bac34bdb033b9ae2771
sptcnl <sptcnl@gmail.com>
2024-09-08 23:25:52 +0900

    added product, like_users DB

--- products/models.py
+++ products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Product(models.Model):
+    flavor = models.CharField(max_length=50)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to="static/image/flavor/", blank=True)
+    like_users = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="like_products"
+    )
+
+    def __str__(self):
+        return self.title
