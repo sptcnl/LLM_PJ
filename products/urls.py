66f770c705408bae34dcf8b3c08ac62879165d1f
sptcnl <sptcnl@gmail.com>
2024-09-09 00:41:47 +0900

    update: added product registration feature

--- products/urls.py
+++ products/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "products"
 
 urlpatterns = [
-    path("<int:pk>/", views.article_detail, name="article_detail"),
+    path("<int:pk>/", views.product_detail, name="product_detail"),
     path("create/", views.create, name="create"),
     path("<int:pk>/delete/", views.delete, name="delete"),
     path("<int:pk>/update/", views.update, name="update"),
--- products/views.py
+++ products/views.py
@@ -1,15 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import (
+    render, 
+    redirect
+)
+from .forms import ProductForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
 
 def index(request):
     return render(request, "products/index.html")
 
 
-def article_detail(request):
+def product_detail(request):
     pass
 
 
+@login_required
 def create(request):
-    pass
+    if request.user.is_staff:
+        if request.method == "POST":
+            form = ProductForm(request.POST, request.FILES)
+            if form.is_valid():
+                product = form.save(commit=False)
+                product.save()
+                return redirect("index")
+        else:
+            form = ProductForm()
+        context = {"form": form}
+        return render(request, "products/create.html", context)
+    messages.error(request, "접근이 불가합니다.")
+    return redirect("index")
 
 
 def delete(request):
