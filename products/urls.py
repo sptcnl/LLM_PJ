06a34df5e1909cafc7241d82570216464f88d363
sptcnl <sptcnl@gmail.com>
2024-09-08 22:29:16 +0900

    Ready to add features

--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = "products"
 
 urlpatterns = [
-    
+    path("<int:pk>/", views.article_detail, name="article_detail"),
+    path("create/", views.create, name="create"),
+    path("<int:pk>/delete/", views.delete, name="delete"),
+    path("<int:pk>/update/", views.update, name="update"),
 ]
--- products/views.py
+++ products/views.py
@@ -2,3 +2,19 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "products/index.html")
+
+
+def article_detail(request):
+    pass
+
+
+def create(request):
+    pass
+
+
+def delete(request):
+    pass
+
+
+def update(request):
+    pass
