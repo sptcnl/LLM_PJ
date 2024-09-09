24eab370463c6425291040a3ebf80e5943306885
nyong <sptcnl@gmail.com>
2024-09-09 17:25:46 +0900

    del: delete profile picture feature

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models
 
 class Users(AbstractUser):
-    profile_picture = models.ImageField(upload_to='profile/', blank=True, default="default/default_profile.png")
+    pass
